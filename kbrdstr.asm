2c15abb239fdb7cd50ecee6254197dc8c2a86472
George Fountis <118578531+fountig@users.noreply.github.com>
2023-08-10 15:19:29 +0300

    Update kbrdstr.asm

--- kbrdstr.asm
+++ kbrdstr.asm
@@ -1,3 +1,15 @@
+; Read a string from the keyboard and store it character by character beginning at &9C90.
+; Terminate if user presses ENTER.
+; Heavily commented, cause I'm a noob :-).
+;
+; e.g. if  user presses 'Hello', then :
+; 
+; &9C90 H
+; &9C91 e
+; &9C92 l
+; &9C93 l
+; &9C94 o
+
 ORG &9C40
 
 txt_output   EQU &BB5A 
@@ -6,8 +18,8 @@ txt_cur_on   EQU &BB81
 
 CALL txt_cur_on ; enable the text cursor. 
 LD HL, &9C90 ; Where our string will be stored.
-JP read_string
 
+JP read_string
 RET
 
 read_string:
