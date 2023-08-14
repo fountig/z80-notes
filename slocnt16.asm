11af757b84ecb347c24e9b169a8e916dbe19a9ae
George Fountis <118578531+fountig@users.noreply.github.com>
2023-08-14 18:58:39 +0300

    Update slocnt16.asm

--- slocnt16.asm
+++ slocnt16.asm
@@ -1,7 +1,7 @@
 ; counter16, counts from 0000 to FFFF. 
 ; DE=counter, HL=memory address. 
 ; 
-; After spending a couple of unsuccesful hours, trying to write my own, I came across this:
+; After spending a couple of unsuccesful hours, going into a different rabbit hole than using a 16-bit register and decrementing it,  I came across this:
 ; https://map.grauw.nl/articles/fast_loops.php. The example is using ld a,d followed by or e to 
 ; check whether the loop is finished, an ingenius way, since you can't use CP nn. It's also quite slow
 ; so slow in fact there is a discernible delay after you press call xxxx and ENTER, and receiving
