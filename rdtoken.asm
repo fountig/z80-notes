c9d93a2368fe2f65e9ac243336ae64c7b212f7e8
George Fountis <118578531+fountig@users.noreply.github.com>
2023-08-15 20:55:44 +0300

    Update rdtoken.asm
    one byte jump table experiment added

--- rdtoken.asm
+++ rdtoken.asm
@@ -1,13 +1,33 @@
 ; We setup a text buffer at &9c40 using the &BD5E call.
-; 
-ORG &4000
 
-cur_token_address equ &4096
+
+ 
+org &5030
+
+; jump table experiment
+LIT_INT_ZER defb &C9
+LIT_INT_ONE defb &C9
+LIT_INT_TWO defb &C9
+LIT_INT_THR defb &C9
+LIT_INT_FOU defb &C9
+LIT_INT_FIV defb &C9
+LIT_INT_SIX defb &C9
+LIT_INT_SEV defb &C9
+LIT_INT_EIG defb &C9
+LIT_INT_NIN defb &C9
+
+
+
+ORG &4100
+
+cur_token_address equ &4700
 
 ;  Firmware calls we need. 
 LD DE, cur_token_address
-LD HL, &9C40
+
+LD HL, &4800
 CALL &BD5E
+
 
 ; parse the buffer text. Let's try reading until we hit an ASCII code of '20'
 
@@ -20,16 +40,29 @@ get_next_token:
 	cp &20
 	jp z, parse_token
 	ld (de), a
-	inc hl
+	inc hl 
 	inc de
 	jp nz, get_next_token
-
+	
 
 
 
 parse_token:
 	; jump table ?
+	; &80nn for every ASCII character
+	; undefined tokens  do a 'JP error'
+	; defined tokens, such as integers from 0 to 9, do a ld (mystack), digit
+	; you can use an offset to calculate the jump addres, it probably wont be the ASCII code.
+	
+	
+	dec de
+	ld h, &50
+	ld a, (de)
+	ld l, a
+	jp (hl)
+	
+ret
 
-	ret
+
 
 
