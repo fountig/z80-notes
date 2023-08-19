f3ade1b7a8314cab21d7cb6d2e083491d6a389cd
George Fountis <118578531+fountig@users.noreply.github.com>
2023-08-19 19:43:24 +0300

    Update hash.asm
    ascii addition works, modulo does, but can't figure out a way to get the previous value from the one that generates a carry and an exit.

--- hash.asm
+++ hash.asm
@@ -5,20 +5,45 @@ org &4000
 ;ld hl, &017f
 ;ld bc, &ff
 
-ld hl, &3e8
-ld bc, &19
+;ld hl, &3e8
+;ld bc, &19
 
-sbc hl, bc ; do the first division outside of loop, so we can begin the loop with 'pop hl'
-	   ; otherwise if we have only push within the loop, the stack gets filled with 
-           ; all the previous computations, and 'ret' jumps to a random address.
-push hl    
+key1 defb 'SWAP',0
+HASH_ADDR equ &8888
+ld a,0
+ld de, key1
+ld hl, &0000
+
+; compute the ascii sum of consecutive memory addresses i.e. defb
+ascii_sum:
+	  ld a, (de) ; store char for loop control (if a=255 exit)
+	  ld (&5555), a  ; move char to temp addr
+	  ld bc, (&5555) ; move temp addr char to bc so we can add. 
+
+	  add hl, bc ; sum will be in HL
+	  inc de
+	  cp 0 
+	  jp nz,ascii_sum
+
+; HL should be 013B
+
+
+; compute the hash by doing ascii_sum MOD 255 (255 will be the length of the hashtable)
+ld hl, &1fe
+ld bc, &ff
+;sbc hl, bc 
 
 compute_modulo:
-	pop hl
 	sbc hl, bc
 	push hl
 	jp nc, compute_modulo
+	jp c, store_hash
 	
 
-pop hl ; we want the one before the carry bit was flipped.
+; this should be 3C from the example.
+
+store_hash:
+	pop hl ; we want the one before the carry bit was flipped.
+	ld (HASH_ADDR), hl
+
 ret
