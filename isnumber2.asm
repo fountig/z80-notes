401a09dc7cdd0da1f0adc2c6909614f9960fbc4c
george <118578531+shadowrun96@users.noreply.github.com>
2023-08-27 00:54:29 +0300

    Update isnumber2.asm
    Added 'sub &c6' so as to not use B as temp. As a result, BC is not corrupted.

--- isnumber2.asm
+++ isnumber2.asm
@@ -2,6 +2,8 @@
 ;; Entry: A contains ASCII character. 
 ;; Exit: A is corrupted. Flags are affected. Carry flag is 1 if its not a number, 0 if it is.
 
+
+
 isnumber:
 	;; Reset Carry flag 
 	scf
@@ -10,7 +12,6 @@ isnumber:
 	;; add &c6 to A. If its more than &39, then there will be a carry. 
 	;; But this doesn't work for lower bounds, for example, if A = 20, then it won't wrap
 
-	ld b,a ;; save A for _lower_bound_check.
 	add &c6
 	call nc, _lower_bound_check
 	ret
@@ -18,6 +19,7 @@ isnumber:
 	;; so if it doesn't wrap, we need to check the possibility of the ASCII code being 00 to 20. 
 	;; We subtract 30 (our lowest possible accepted value). If it wraps, the carry is set to 1.
 	_lower_bound_check:
-		ld a, b
+		
+		sub &c6 ;; restore A's value. 
 		sub &30
 		ret
