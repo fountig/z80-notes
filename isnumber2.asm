216dafb040f837ba4b74d5926b7892f8d09fbaa2
george <118578531+shadowrun96@users.noreply.github.com>
2023-08-26 19:59:28 +0300

    Create isnumber2.asm

---
+++ isnumber2.asm
@@ -0,0 +1,27 @@
+;; subroutine to check if ASCII VALUE in A is a decimal integer number between 0 - 9.
+;; Entry: A contains ASCII character. 
+;; Exit: A is corrupted. Flags are affected. Carry flag is 1 if its not a number, 0 if it is.
+
+isnumber:
+
+;; Reset Carry flag 
+	scf
+	ccf
+
+	;; add &c6 to A. If its more than &39, then there will be a carry. 
+	;; But this doesn't work for lower bounds, for example, if A = 20, then it won't wrap
+
+	ld b,a ;; save A for _lower_bound_check.
+	ld a, &96
+	add &c6
+	call nc, _lower_bound_check
+	ret c
+
+	;; so if it doesn't wrap, we need to check the possibility of the ASCII code being 00 to 20. 
+	;; We subtract 30 (our lowest possible accepted value). If it wraps, the carry is set to 1.
+	_lower_bound_check:
+		ld a, b
+		sub &30
+		ret
+
+;; PS: Compare this with isnumber.asm, to see the difference. 
